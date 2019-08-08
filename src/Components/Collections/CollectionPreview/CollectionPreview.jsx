9f543af3d62409213825e13db0ddd037b235203c
Rohit Islam <air.rohit999@gmail.com>
2019-08-08 14:20:42 +0000

    collection item added, collection preview and shop component updated

--- src/Components/Collections/CollectionPreview/CollectionPreview.jsx
+++ src/Components/Collections/CollectionPreview/CollectionPreview.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./CollectionPreview.scss";
+import CollectionItem from "../CollectionItem/CollectionItem";
 
 const CollectionPreview = props => {
   return (
@@ -8,8 +9,8 @@ const CollectionPreview = props => {
       <div className="preview">
         {props.items
           .filter((item, index) => index < 4)
-          .map(item => (
-            <div key={item.id}> {item.name} </div>
+          .map(({ id, ...otherItemProps }) => (
+            <CollectionItem key={id} {...otherItemProps} />
           ))}
       </div>
     </div>
